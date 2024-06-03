c11cb1605fd0d336595539a2c94fa2497036aaf2
Oleksandr-Korniienko514 <bon.bon199993@gmail.com>
2024-06-04 00:51:12 +0200

    HOMEWORK3 UPDATE

--- js/task-2.js
+++ js/task-2.js
@@ -7,24 +7,54 @@
 // }
 
 
-// console.log(getShippingMessage("Australia", 120, 50)); 
-// console.log(getShippingMessage("Germany", 80, 20)); 
-// console.log(getShippingMessage("Sweden", 100, 20)); 
+// console.log(getShippingMessage("Australia", 120, 50));
+// console.log(getShippingMessage("Germany", 80, 20));
+// console.log(getShippingMessage("Sweden", 100, 20));
 
 // -----------------------------------------------------------
 
-function formatMessage(message, maxLength) {
-    if (message.length <= maxLength) {
-        return message;
-    } else {
-        return message.slice(0, maxLength) + "...";
-    }
+// function formatMessage(message, maxLength) {
+//     if (message.length <= maxLength) {
+//         return message;
+//     } else {
+//         return message.slice(0, maxLength) + "...";
+//     }
+// }
+
+
+// console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+// console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+// console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+// console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
+// ------------------------------------------------------------------------------------------------------------------------------------------
+
+// !HOMEWORK 3
+
+function makeArray (firstArray, secondArray, maxLength) {
+    
+let thirdArray = [];
+
+for (let i = 0; i < firstArray.length; i+=1) {
+    thirdArray.push(firstArray[i]);
+}
+
+for (let i = 0; i < secondArray.length; i+=1) {
+    thirdArray.push(secondArray[i]);
+}
+
+if (thirdArray.length > maxLength) {
+    thirdArray = thirdArray.slice(0, maxLength);
+}
+    return thirdArray;
 }
 
 
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,8 @@
 // function getElementWidth(content, padding, border) {
 
 //     const contentWidth = parseFloat(content);
-//     const paddingWidth = parseFloat(padding) * 2; 
-//     const borderWidth = parseFloat(border) * 2; 
+//     const paddingWidth = parseFloat(padding) * 2;
+//     const borderWidth = parseFloat(border) * 2;
 
 //     const totalWidth = contentWidth + paddingWidth + borderWidth;
 
@@ -15,16 +15,40 @@
 
 // ------------------------------------------------------------------
 
-function checkForSpam(message) {
-    const CaseMessage = message.toLowerCase(); 
-    return CaseMessage.includes('spam') || CaseMessage.includes('sale');
+// function checkForSpam(message) {
+//     const CaseMessage = message.toLowerCase();
+//     return CaseMessage.includes('spam') || CaseMessage.includes('sale');
+// }
+
+
+// console.log(checkForSpam("Latest technology news")); // false
+// console.log(checkForSpam("JavaScript weekly newsletter")); // false
+// console.log(checkForSpam("Get best sale offers now!")); // true
+// console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+// console.log(checkForSpam("Trust me, this is not a spam message")); // true
+// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+// -------------------------------------------------------------------------------------------------------
+
+// !HOMEWORK 3
+
+function filterArray(numbers, value) {
+
+let numberHigherValues = [];
+
+for (let i = 0; i < numbers.length; i += 1) {
+
+if (numbers[i] > value) {
+numberHigherValues.push(numbers[i]);
+}
+    }
+return numberHigherValues;
 }
 
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
