030f5eb45fa51153d488b88344aec0ad5bdcb476
Oleksandr-Korniienko514 <bon.bon199993@gmail.com>
2024-06-17 22:55:15 +0200

    UPDHOMEWORK5

--- js/task-4.js
+++ js/task-4.js
@@ -26,3 +26,57 @@
 // console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
 // console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
 // console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
+
+
+// !HOMEWORK 5
+
+const getTotalBalanceByGender = (users, gender) => {
+	const total = users
+		.filter((user) => user.gender === gender)
+		.map((user) => user.balance)
+		.reduce((total, user) => (total += user), 0);
+
+	return total;
+};
+
+const clients = [
+	{
+		name: "Moore Hensley",
+		gender: "male",
+		balance: 2811,
+	},
+	{
+		name: "Sharlene Bush",
+		gender: "female",
+		balance: 3821,
+	},
+	{
+		name: "Ross Vazquez",
+		gender: "male",
+		balance: 3793,
+	},
+	{
+		name: "Elma Head",
+		gender: "female",
+		balance: 2278,
+	},
+	{
+		name: "Carey Barr",
+		gender: "male",
+		balance: 3951,
+	},
+	{
+		name: "Blackburn Dotson",
+		gender: "male",
+		balance: 1498,
+	},
+	{
+		name: "Sheree Anthony",
+		gender: "female",
+		balance: 2764,
+	},
+];
+
+console.log(getTotalBalanceByGender(clients, "male")); // 12053
+
+console.log(getTotalBalanceByGender(clients, "female")); // 8863
